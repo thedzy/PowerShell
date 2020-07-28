aa9e2c4dece70e57ff8476b42baebf59ebf3659d
thedzy <thedzy@hotmail.com>
2020-07-28 20:46:55 +0200

    Inital commit

---
+++ snippets/root_privilages_required.ps1
@@ -0,0 +1,16 @@
+
+# Get the current user
+$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+# Check the current role of the user
+$admin_status = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+
+# If not and admin, elevate
+if ( -not  $admin_status ) {
+    write-host "Signing into admin"
+    Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($MyInvocation.MyCommand.Definition))
+    exit $LASTEXITCODE
+}
+
+Write-Host "Do stuff"
+
+exit
