275bf04e809a3bd1340b3dbccb8ef4b903ad2f24
thedzy <thedzy@hotmail.com>
2020-08-02 16:50:48 +0200

    Dispalys the length of password

--- snippets/get_user_input.ps1
+++ snippets/get_user_input.ps1
@@ -3,7 +3,7 @@
 $username = ( Read-Host "Username" )
 # Masked inout
 $password = ( Read-Host -AsSecureString "Password" )
-Write-Host "`n$username : ********"
+Write-Host ("`n{0} : {1}" -f $username , ('*' * $password.length))
 
 # Single key input
 Write-Host -NoNewline "Press any key "
